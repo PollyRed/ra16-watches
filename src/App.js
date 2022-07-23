70b91563a3bac41aa7bc87f7f1c82ccf689b8c6c
Polina Titova <titova_polina@mail.ru>
2022-07-24 00:20:23 +0300

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import {nanoid} from 'nanoid';
+import AddWatchesForm from './components/AddWatchesForm';
+import WatchesList from './components/WatchesList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+      data: [
+          {name: 'London', timezone: '3', id: nanoid()}
+      ]
+  }
+
+  addItem = (body) => {
+
+      const newItem = {
+          name: body.name,
+          timezone: body.timezone,
+          id: nanoid()
+      }
+
+      this.setState(({data})=> {
+          const newArr = [...data, newItem];
+          return {
+              data: newArr
+          }
+      })
+  }
+
+  deleteItem = (id) => {
+      this.setState(({data}) => {
+
+          const index = data.findIndex(elem => elem.id === id);
+          const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+
+          return {
+              data: newArr
+          }
+      })
+  }
+
+  render() {
+      return (
+          <div className="App">
+              <AddWatchesForm onAdd={this.addItem}/>
+              <WatchesList watches={this.state.data} onDelete={this.deleteItem}/>
+          </div>
+      )
+  }
 }
 
 export default App;
